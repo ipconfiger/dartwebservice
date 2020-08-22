6a29e9c58dc54c2eaea7d99b8ccdf8b4f30b1db3
ipconfiger <superpowerlee@gmail.com>
2020-08-22 19:20:06 +0800

    增加了对嵌入类型的支持（暂时只支持单级）

--- lib/dartwebservice.dart
+++ lib/dartwebservice.dart
@@ -2,13 +2,13 @@ library dartwebservice;
 
 import 'package:xml/xml.dart';
 
-class InvalidDefinationException implements Exception { 
+class InvalidDefinationException implements Exception {
   String error;
   InvalidDefinationException(this.error);
-  String errMsg() => this.error; 
-} 
+  String errMsg() => this.error;
+}
 
-class TransParameter{
+class TransParameter {
   String parameterName;
   String typeDefinition;
   TransParameter(this.parameterName, this.typeDefinition);
@@ -16,9 +16,18 @@ class TransParameter{
 
 class TransType {
   String typeName;
+  bool isComplex;
   List<TransParameter> parameters;
-  TransType(this.typeName){
+  TransType(this.typeName) {
     this.parameters = new List<TransParameter>();
+  }
+}
+
+class ComplexType {
+  String typeName;
+  List<TransParameter> propertys;
+  ComplexType(this.typeName) {
+    this.propertys = new List<TransParameter>();
   }
 }
 
@@ -32,69 +41,124 @@ class WebService {
   String wsdl;
   XmlDocument difinations;
   Map<String, TransType> types;
+  Map<String, ComplexType> complexTypes;
   List<Interface> interfacies;
   WebService.fromWsdl(this.wsdl) {
     this.difinations = XmlDocument.parse(this.wsdl);
     this.types = new Map<String, TransType>();
     this.interfacies = new List<Interface>();
+    this.complexTypes = new Map<String, ComplexType>();
   }
 
+  String makeSoap(
+      Map<String, String> parameters, Map<String, String> payload) {}
+
   void execute() {
-    this.difinations.descendants.forEach((node) { 
+    this.difinations.descendants.forEach((node) {
       // 解析类型
-      if (node.toString().startsWith('<wsdl:message')){
+      if (node.toString().startsWith('<wsdl:types')) {
+        final typeList = node.firstElementChild.children.toList();
+        //print('detect types!!:${typeList.toList()}');
+        for (var el in typeList) {
+          if (el.nodeType == XmlNodeType.TEXT) {
+            continue;
+          }
+          final typeName = el.getAttribute('name');
+          print('type name:$typeName');
+          final tp = ComplexType(typeName);
+          if (el.firstElementChild == null) {
+            continue;
+          }
+          final propertyList =
+              el.firstElementChild.firstElementChild.children.toList();
+          for (var property in propertyList) {
+            if (property.nodeType == XmlNodeType.TEXT) {
+              continue;
+            }
+            final propertyName = property.getAttribute('name');
+            final propertyType = property.getAttribute('type');
+            final p = new TransParameter(propertyName, propertyType);
+            tp.propertys.add(p);
+          }
+          this.complexTypes[typeName] = tp;
+        }
+      }
+    });
+    print('complex types:${this.complexTypes}');
+    this.difinations.descendants.forEach((node) {
+      // 解析接口
+      if (node.toString().startsWith('<wsdl:message')) {
         final name = node.getAttribute('name');
-        print('type: $name');
+        //print('type: $name');
         final tp = TransType(name);
-        node.children.forEach((subnode){
-          if (subnode.toString().startsWith('<wsdl:part')){
-            print('\t property:${subnode.getAttribute('name')}[${subnode.getAttribute('type')}]');
-            final p = new TransParameter(subnode.getAttribute('name'), subnode.getAttribute('type'));
-            tp.parameters.add(p);
+        node.children.forEach((subnode) {
+          if (subnode.toString().startsWith('<wsdl:part')) {
+            if (subnode.getAttribute('element') != null) {
+              final elementName = subnode.getAttribute('element').split(':')[1];
+              print('part name:$elementName');
+              if (this.complexTypes.containsKey(elementName)) {
+                tp.isComplex = true;
+                tp.parameters.addAll(this.complexTypes[elementName].propertys);
+              }
+            } else {
+              final p = new TransParameter(
+                  subnode.getAttribute('name'), subnode.getAttribute('type'));
+              tp.parameters.add(p);
+            }
           }
         });
-        this.types.addAll({
-          name: tp
-        });
+        this.types.addAll({name: tp});
       }
     });
     // 解析端口
-    this.difinations.descendants.forEach((node) { 
-      if (node.toString().startsWith('<wsdl:portType')){
-         node.children.forEach((child){
-           if (child.toString().startsWith('<wsdl:operation')){
-             final name = child.getAttribute('name');
-             final paramsOrder = child.getAttribute('parameterOrder');
-             print('interface $name, params:$paramsOrder');
-             Interface ins = new Interface();
-             ins.interfaceName = name;
-             child.children.forEach((sub){
-               if (sub.toString().startsWith('<wsdl:input')) {
-                 // 获取到输入定义
-                 final inputName = sub.getAttribute('message').split(':')[1];
-                 if (!this.types.containsKey(inputName)){
-                   throw new InvalidDefinationException('Message $inputName not found');
-                 }
-                 ins.inputs = this.types[inputName];
-                 print('inputtype: $inputName');
-               }
-               if (sub.toString().startsWith('<wsdl:output')) {
-                 // 获取到输出定义
-                 final outName = sub.getAttribute('message').split(':')[1];
-                 if (!this.types.containsKey(outName)){
-                   throw new InvalidDefinationException('Message $outName not found');
-                 }
-                 ins.outputs = this.types[outName];
-                 print('outputtype: $outName');
-               }
-             });
-             interfacies.add(ins);
-           }
-         });
+    this.difinations.descendants.forEach((node) {
+      if (node.toString().startsWith('<wsdl:portType')) {
+        node.children.forEach((child) {
+          if (child.toString().startsWith('<wsdl:operation')) {
+            final name = child.getAttribute('name');
+            final paramsOrder = child.getAttribute('parameterOrder');
+            print('interface $name, params:$paramsOrder');
+            Interface ins = new Interface();
+            ins.interfaceName = name;
+            child.children.forEach((sub) {
+              if (sub.toString().startsWith('<wsdl:input')) {
+                // 获取到输入定义
+                final inputName = sub.getAttribute('message').split(':')[1];
+                if (!this.types.containsKey(inputName)) {
+                  throw new InvalidDefinationException(
+                      'Message $inputName not found');
+                }
+                ins.inputs = this.types[inputName];
+                print('inputtype: $inputName');
+              }
+              if (sub.toString().startsWith('<wsdl:output')) {
+                // 获取到输出定义
+                final outName = sub.getAttribute('message').split(':')[1];
+                if (!this.types.containsKey(outName)) {
+                  throw new InvalidDefinationException(
+                      'Message $outName not found');
+                }
+                ins.outputs = this.types[outName];
+                print('outputtype: $outName');
+              }
+            });
+            interfacies.add(ins);
+          }
+        });
       }
     });
-
+    this.interfacies.forEach((interface) {
+      print('interface:${interface.interfaceName}');
+      print('input type:${interface.inputs.typeName}');
+      interface.inputs.parameters.forEach((param) {
+        print(
+            '\t param name:${param.parameterName} or type:${param.typeDefinition}');
+      });
+      print('output type:${interface.outputs.typeName}');
+      interface.outputs.parameters.forEach((param) {
+        print(
+            '\t param name:${param.parameterName} or type:${param.typeDefinition}');
+      });
+    });
   }
-  
-
 }
--- test/dartwebservice_test.dart
+++ test/dartwebservice_test.dart
@@ -10,349 +10,203 @@ void main() {
   });
 }
 
-final wsdlString = '''<?xml version="1.0" encoding="UTF-8"?>
-<wsdl:definitions targetNamespace="http://192.168.100.26/pnweb/services/TmriOutNewAccess" xmlns:apachesoap="http://xml.apache.org/xml-soap" xmlns:impl="http://192.168.100.26/pnweb/services/TmriOutNewAccess" xmlns:intf="http://192.168.100.26/pnweb/services/TmriOutNewAccess" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/" xmlns:wsdlsoap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-<!--WSDL created by Apache Axis version: 1.4
-Built on Apr 22, 2006 (06:55:48 PDT)-->
-
-   <wsdl:message name="writeObjectOutNewResponse">
-
-      <wsdl:part name="writeObjectOutNewReturn" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:message name="writeObjectOutResponse">
-
-      <wsdl:part name="writeObjectOutReturn" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:message name="queryObjectOutNewRequest">
-
-      <wsdl:part name="xtlb" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkxlh" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkid" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="cjsqbh" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwjgdm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwmc" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhbz" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhxm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="zdbs" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="UTF8XmlDoc" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:message name="queryObjectOutResponse">
-
-      <wsdl:part name="queryObjectOutReturn" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:message name="queryObjectOutRequest">
-
-      <wsdl:part name="xtlb" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkxlh" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkid" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhbz" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwmc" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwjgdm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhxm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="zdbs" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="UTF8XmlDoc" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:message name="queryObjectOutNewResponse">
-
-      <wsdl:part name="queryObjectOutNewReturn" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:message name="writeObjectOutNewRequest">
-
-      <wsdl:part name="xtlb" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkxlh" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkid" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="cjsqbh" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwjgdm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwmc" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhbz" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhxm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="zdbs" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="UTF8XmlDoc" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:message name="writeObjectOutRequest">
-
-      <wsdl:part name="xtlb" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkxlh" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="jkid" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhbz" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwmc" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="dwjgdm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="yhxm" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="zdbs" type="xsd:string">
-
-      </wsdl:part>
-
-      <wsdl:part name="UTF8XmlDoc" type="xsd:string">
-
-      </wsdl:part>
-
-   </wsdl:message>
-
-   <wsdl:portType name="TmriJaxRpcOutNewAccess">
-
-      <wsdl:operation name="queryObjectOut" parameterOrder="xtlb jkxlh jkid yhbz dwmc dwjgdm yhxm zdbs UTF8XmlDoc">
-
-         <wsdl:input message="impl:queryObjectOutRequest" name="queryObjectOutRequest">
-
-       </wsdl:input>
-
-         <wsdl:output message="impl:queryObjectOutResponse" name="queryObjectOutResponse">
-
-       </wsdl:output>
-
-      </wsdl:operation>
-
-      <wsdl:operation name="queryObjectOutNew" parameterOrder="xtlb jkxlh jkid cjsqbh dwjgdm dwmc yhbz yhxm zdbs UTF8XmlDoc">
-
-         <wsdl:input message="impl:queryObjectOutNewRequest" name="queryObjectOutNewRequest">
-
-       </wsdl:input>
-
-         <wsdl:output message="impl:queryObjectOutNewResponse" name="queryObjectOutNewResponse">
-
-       </wsdl:output>
-
-      </wsdl:operation>
-
-      <wsdl:operation name="writeObjectOut" parameterOrder="xtlb jkxlh jkid yhbz dwmc dwjgdm yhxm zdbs UTF8XmlDoc">
-
-         <wsdl:input message="impl:writeObjectOutRequest" name="writeObjectOutRequest">
-
-       </wsdl:input>
-
-         <wsdl:output message="impl:writeObjectOutResponse" name="writeObjectOutResponse">
-
-       </wsdl:output>
-
-      </wsdl:operation>
-
-      <wsdl:operation name="writeObjectOutNew" parameterOrder="xtlb jkxlh jkid cjsqbh dwjgdm dwmc yhbz yhxm zdbs UTF8XmlDoc">
-
-         <wsdl:input message="impl:writeObjectOutNewRequest" name="writeObjectOutNewRequest">
-
-       </wsdl:input>
-
-         <wsdl:output message="impl:writeObjectOutNewResponse" name="writeObjectOutNewResponse">
-
-       </wsdl:output>
-
-      </wsdl:operation>
-
-   </wsdl:portType>
-
-   <wsdl:binding name="TmriOutNewAccessSoapBinding" type="impl:TmriJaxRpcOutNewAccess">
-
-      <wsdlsoap:binding style="rpc" transport="http://schemas.xmlsoap.org/soap/http"/>
-
-      <wsdl:operation name="queryObjectOut">
-
-         <wsdlsoap:operation soapAction=""/>
-
-         <wsdl:input name="queryObjectOutRequest">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://endpoint.axis.framework.tmri.com" use="encoded"/>
-
-         </wsdl:input>
-
-         <wsdl:output name="queryObjectOutResponse">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://192.168.100.26/pnweb/services/TmriOutNewAccess" use="encoded"/>
-
-         </wsdl:output>
-
-      </wsdl:operation>
-
-      <wsdl:operation name="queryObjectOutNew">
-
-         <wsdlsoap:operation soapAction=""/>
-
-         <wsdl:input name="queryObjectOutNewRequest">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://endpoint.axis.framework.tmri.com" use="encoded"/>
-
-         </wsdl:input>
-
-         <wsdl:output name="queryObjectOutNewResponse">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://192.168.100.26/pnweb/services/TmriOutNewAccess" use="encoded"/>
-
-         </wsdl:output>
-
-      </wsdl:operation>
-
-      <wsdl:operation name="writeObjectOut">
-
-         <wsdlsoap:operation soapAction=""/>
-
-         <wsdl:input name="writeObjectOutRequest">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://endpoint.axis.framework.tmri.com" use="encoded"/>
-
-         </wsdl:input>
-
-         <wsdl:output name="writeObjectOutResponse">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://192.168.100.26/pnweb/services/TmriOutNewAccess" use="encoded"/>
-
-         </wsdl:output>
-
-      </wsdl:operation>
-
-      <wsdl:operation name="writeObjectOutNew">
-
-         <wsdlsoap:operation soapAction=""/>
-
-         <wsdl:input name="writeObjectOutNewRequest">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://endpoint.axis.framework.tmri.com" use="encoded"/>
-
-         </wsdl:input>
-
-         <wsdl:output name="writeObjectOutNewResponse">
-
-            <wsdlsoap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="http://192.168.100.26/pnweb/services/TmriOutNewAccess" use="encoded"/>
-
-         </wsdl:output>
-
-      </wsdl:operation>
-
-   </wsdl:binding>
-
-   <wsdl:service name="TmriJaxRpcOutNewAccessService">
-
-      <wsdl:port binding="impl:TmriOutNewAccessSoapBinding" name="TmriOutNewAccess">
-
-         <wsdlsoap:address location="http://192.168.100.26/pnweb/services/TmriOutNewAccess"/>
-
-      </wsdl:port>
-
-   </wsdl:service>
-
+final wsdlString = '''<?xml version="1.0" encoding="utf-8"?>
+<wsdl:definitions xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:tm="http://microsoft.com/wsdl/mime/textMatching/" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:mime="http://schemas.xmlsoap.org/wsdl/mime/" xmlns:tns="http://tempuri.org/" xmlns:s="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://schemas.xmlsoap.org/wsdl/soap12/" xmlns:http="http://schemas.xmlsoap.org/wsdl/http/" targetNamespace="http://tempuri.org/" xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/">
+  <wsdl:types>
+    <s:schema elementFormDefault="qualified" targetNamespace="http://tempuri.org/">
+      <s:element name="query">
+        <s:complexType>
+          <s:sequence>
+            <s:element minOccurs="0" maxOccurs="1" name="xmlDoc" type="s:string" />
+          </s:sequence>
+        </s:complexType>
+      </s:element>
+      <s:element name="queryResponse">
+        <s:complexType>
+          <s:sequence>
+            <s:element minOccurs="0" maxOccurs="1" name="queryResult" type="s:string" />
+          </s:sequence>
+        </s:complexType>
+      </s:element>
+      <s:element name="write">
+        <s:complexType>
+          <s:sequence>
+            <s:element minOccurs="0" maxOccurs="1" name="xmlDoc" type="s:string" />
+          </s:sequence>
+        </s:complexType>
+      </s:element>
+      <s:element name="writeResponse">
+        <s:complexType>
+          <s:sequence>
+            <s:element minOccurs="0" maxOccurs="1" name="writeResult" type="s:string" />
+          </s:sequence>
+        </s:complexType>
+      </s:element>
+      <s:element name="string" nillable="true" type="s:string" />
+    </s:schema>
+  </wsdl:types>
+  <wsdl:message name="querySoapIn">
+    <wsdl:part name="parameters" element="tns:query" />
+  </wsdl:message>
+  <wsdl:message name="querySoapOut">
+    <wsdl:part name="parameters" element="tns:queryResponse" />
+  </wsdl:message>
+  <wsdl:message name="writeSoapIn">
+    <wsdl:part name="parameters" element="tns:write" />
+  </wsdl:message>
+  <wsdl:message name="writeSoapOut">
+    <wsdl:part name="parameters" element="tns:writeResponse" />
+  </wsdl:message>
+  <wsdl:message name="queryHttpGetIn">
+    <wsdl:part name="xmlDoc" type="s:string" />
+  </wsdl:message>
+  <wsdl:message name="queryHttpGetOut">
+    <wsdl:part name="Body" element="tns:string" />
+  </wsdl:message>
+  <wsdl:message name="writeHttpGetIn">
+    <wsdl:part name="xmlDoc" type="s:string" />
+  </wsdl:message>
+  <wsdl:message name="writeHttpGetOut">
+    <wsdl:part name="Body" element="tns:string" />
+  </wsdl:message>
+  <wsdl:message name="queryHttpPostIn">
+    <wsdl:part name="xmlDoc" type="s:string" />
+  </wsdl:message>
+  <wsdl:message name="queryHttpPostOut">
+    <wsdl:part name="Body" element="tns:string" />
+  </wsdl:message>
+  <wsdl:message name="writeHttpPostIn">
+    <wsdl:part name="xmlDoc" type="s:string" />
+  </wsdl:message>
+  <wsdl:message name="writeHttpPostOut">
+    <wsdl:part name="Body" element="tns:string" />
+  </wsdl:message>
+  <wsdl:portType name="VeptsOutAccessSoap">
+    <wsdl:operation name="query">
+      <wsdl:input message="tns:querySoapIn" />
+      <wsdl:output message="tns:querySoapOut" />
+    </wsdl:operation>
+    <wsdl:operation name="write">
+      <wsdl:input message="tns:writeSoapIn" />
+      <wsdl:output message="tns:writeSoapOut" />
+    </wsdl:operation>
+  </wsdl:portType>
+  <wsdl:portType name="VeptsOutAccessHttpGet">
+    <wsdl:operation name="query">
+      <wsdl:input message="tns:queryHttpGetIn" />
+      <wsdl:output message="tns:queryHttpGetOut" />
+    </wsdl:operation>
+    <wsdl:operation name="write">
+      <wsdl:input message="tns:writeHttpGetIn" />
+      <wsdl:output message="tns:writeHttpGetOut" />
+    </wsdl:operation>
+  </wsdl:portType>
+  <wsdl:portType name="VeptsOutAccessHttpPost">
+    <wsdl:operation name="query">
+      <wsdl:input message="tns:queryHttpPostIn" />
+      <wsdl:output message="tns:queryHttpPostOut" />
+    </wsdl:operation>
+    <wsdl:operation name="write">
+      <wsdl:input message="tns:writeHttpPostIn" />
+      <wsdl:output message="tns:writeHttpPostOut" />
+    </wsdl:operation>
+  </wsdl:portType>
+  <wsdl:binding name="VeptsOutAccessSoap" type="tns:VeptsOutAccessSoap">
+    <soap:binding transport="http://schemas.xmlsoap.org/soap/http" />
+    <wsdl:operation name="query">
+      <soap:operation soapAction="http://tempuri.org/query" style="document" />
+      <wsdl:input>
+        <soap:body use="literal" />
+      </wsdl:input>
+      <wsdl:output>
+        <soap:body use="literal" />
+      </wsdl:output>
+    </wsdl:operation>
+    <wsdl:operation name="write">
+      <soap:operation soapAction="http://tempuri.org/write" style="document" />
+      <wsdl:input>
+        <soap:body use="literal" />
+      </wsdl:input>
+      <wsdl:output>
+        <soap:body use="literal" />
+      </wsdl:output>
+    </wsdl:operation>
+  </wsdl:binding>
+  <wsdl:binding name="VeptsOutAccessSoap12" type="tns:VeptsOutAccessSoap">
+    <soap12:binding transport="http://schemas.xmlsoap.org/soap/http" />
+    <wsdl:operation name="query">
+      <soap12:operation soapAction="http://tempuri.org/query" style="document" />
+      <wsdl:input>
+        <soap12:body use="literal" />
+      </wsdl:input>
+      <wsdl:output>
+        <soap12:body use="literal" />
+      </wsdl:output>
+    </wsdl:operation>
+    <wsdl:operation name="write">
+      <soap12:operation soapAction="http://tempuri.org/write" style="document" />
+      <wsdl:input>
+        <soap12:body use="literal" />
+      </wsdl:input>
+      <wsdl:output>
+        <soap12:body use="literal" />
+      </wsdl:output>
+    </wsdl:operation>
+  </wsdl:binding>
+  <wsdl:binding name="VeptsOutAccessHttpGet" type="tns:VeptsOutAccessHttpGet">
+    <http:binding verb="GET" />
+    <wsdl:operation name="query">
+      <http:operation location="/query" />
+      <wsdl:input>
+        <http:urlEncoded />
+      </wsdl:input>
+      <wsdl:output>
+        <mime:mimeXml part="Body" />
+      </wsdl:output>
+    </wsdl:operation>
+    <wsdl:operation name="write">
+      <http:operation location="/write" />
+      <wsdl:input>
+        <http:urlEncoded />
+      </wsdl:input>
+      <wsdl:output>
+        <mime:mimeXml part="Body" />
+      </wsdl:output>
+    </wsdl:operation>
+  </wsdl:binding>
+  <wsdl:binding name="VeptsOutAccessHttpPost" type="tns:VeptsOutAccessHttpPost">
+    <http:binding verb="POST" />
+    <wsdl:operation name="query">
+      <http:operation location="/query" />
+      <wsdl:input>
+        <mime:content type="application/x-www-form-urlencoded" />
+      </wsdl:input>
+      <wsdl:output>
+        <mime:mimeXml part="Body" />
+      </wsdl:output>
+    </wsdl:operation>
+    <wsdl:operation name="write">
+      <http:operation location="/write" />
+      <wsdl:input>
+        <mime:content type="application/x-www-form-urlencoded" />
+      </wsdl:input>
+      <wsdl:output>
+        <mime:mimeXml part="Body" />
+      </wsdl:output>
+    </wsdl:operation>
+  </wsdl:binding>
+  <wsdl:service name="VeptsOutAccess">
+    <wsdl:port name="VeptsOutAccessSoap" binding="tns:VeptsOutAccessSoap">
+      <soap:address location="http://192.168.174.2/hbjcxweb/services/VeptsOutAccess.asmx" />
+    </wsdl:port>
+    <wsdl:port name="VeptsOutAccessSoap12" binding="tns:VeptsOutAccessSoap12">
+      <soap12:address location="http://192.168.174.2/hbjcxweb/services/VeptsOutAccess.asmx" />
+    </wsdl:port>
+    <wsdl:port name="VeptsOutAccessHttpGet" binding="tns:VeptsOutAccessHttpGet">
+      <http:address location="http://192.168.174.2/hbjcxweb/services/VeptsOutAccess.asmx" />
+    </wsdl:port>
+    <wsdl:port name="VeptsOutAccessHttpPost" binding="tns:VeptsOutAccessHttpPost">
+      <http:address location="http://192.168.174.2/hbjcxweb/services/VeptsOutAccess.asmx" />
+    </wsdl:port>
+  </wsdl:service>
 </wsdl:definitions>''';
